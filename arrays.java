58183637f405c30f48784fcd146e58d687c6aac6
emdoll <93285260+emdoll@users.noreply.github.com>
2025-10-28 21:06:09 -0500

    agregado datos inesperados
    Added input validation for grades and average check.

--- arrays.java
+++ arrays.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class arrays {
@@ -8,21 +9,44 @@ public class arrays {
         double promedio;
         Scanner entrada = new Scanner(System.in);
         System.out.print("cuantas calificaciones desea agregar? ");
-        calificaciones = entrada.nextInt();
+        while (true) {
+            try {
+                calificaciones = entrada.nextInt();
+                break;
+            } catch (InputMismatchException err) {
+                System.out.println("error");
+                entrada.next();
+                System.out.println("ingrese un numero valido" + err);
+            }
+        }
 
         promedios = new double[calificaciones];
 
         for (int j = 0; j < calificaciones; j++) {
-            System.out.print("introduzca una calificacion: ");
-            promedios[j] = entrada.nextDouble();
+            while (true) {
+                try {
+                    System.out.print("introduzca una calificacion: ");
+                    promedios[j] = entrada.nextDouble();
+                    break;
+                } catch (InputMismatchException er) {
+                    System.out.println("introduzca un numero valido");
+                    entrada.next();
+                }
+            }
         }
-        
         for (int j = 0; j < calificaciones; j++) {
             suma = suma + promedios[j];
         }
         promedio = suma / calificaciones;
         System.out.println(promedio);
+        if (promedio > 3) {
+            System.out.println("aprobado");
+        } else {
+            System.out.println("reprobado");
+        }
+        ;
 
         entrada.close();
     }
 }
+
