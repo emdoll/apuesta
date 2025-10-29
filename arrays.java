688e006a78ffa0589a27a78d793bf6d26d8bd6bd
emdoll <93285260+emdoll@users.noreply.github.com>
2025-10-28 21:21:06 -0500

    Enhance grade calculation with max and min value
    Added functionality to calculate maximum and minimum grades.

--- arrays.java
+++ arrays.java
@@ -6,7 +6,7 @@ public class arrays {
         double[] promedios;
         double suma = 0;
         int calificaciones;
-        double promedio;
+        double promedio, maximo = Double.MIN_VALUE, minimo = Double.MAX_VALUE;
         Scanner entrada = new Scanner(System.in);
         System.out.print("cuantas calificaciones desea agregar? ");
         while (true) {
@@ -27,6 +27,7 @@ public class arrays {
                 try {
                     System.out.print("introduzca una calificacion: ");
                     promedios[j] = entrada.nextDouble();
+
                     break;
                 } catch (InputMismatchException er) {
                     System.out.println("introduzca un numero valido");
@@ -34,11 +35,21 @@ public class arrays {
                 }
             }
         }
+
         for (int j = 0; j < calificaciones; j++) {
+            double numactual = promedios[j];
+            if (numactual > maximo) {
+                maximo = numactual;
+            }
+            if (numactual < minimo) {
+                minimo = numactual;
+            }
             suma = suma + promedios[j];
         }
         promedio = suma / calificaciones;
         System.out.println(promedio);
+        System.out.println("el numero maximo es: " + maximo);
+        System.out.println("el numero minimo es: " + minimo);
         if (promedio > 3) {
             System.out.println("aprobado");
         } else {
